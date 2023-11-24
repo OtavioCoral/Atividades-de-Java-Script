c05154a56ca884a719428ec8771f20e749ce47ec
OtávioCoral <137587307+OtavioCoral@users.noreply.github.com>
2023-11-24 13:24:20 -0300

    Add files via upload

---
+++ typeoff.js
@@ -0,0 +1,40 @@
+// let cidade, nome;
+
+// console.log(typeof cidade)
+// console.log(typeof nome)
+
+// nome = prompt("Qual é o seu nome?")
+// cidade = prompt("Agora me diga onde você mora:")
+
+// console.log(typeof nome)
+// console.log(typeof cidade)
+
+// console.log("Muito prazer, me chamo " + nome + "e eu moro em " + cidade + "!")
+
+// let resposta1 = prompt("Você gosta de programar? (Responda com Sim ou Não)")
+// let resposta2 = prompt("Você já viajou para fora do país? (Responda com Sim ou Não)")
+// let resposta3 = prompt("Você tem animais de estimação? (Responda com Sim ou Não)")
+ 
+// console.log("Pergunta 1: Você gosta de programar? Resposta: " + resposta1)
+// console.log("Pergunta 2: Você já viajou para fora do país? Resposta: " + resposta2)
+// console.log("Pergunta 3: Você tem animais de estimação? Resposta: " + resposta3)
+
+// let x = 5
+// let y = 10
+// let z  
+
+// z = x
+// x = y
+// y = z 
+
+// console.log("Troquei valores x = 10", x)
+// console.log("Troquei valores y = 5", y)
+
+// let numero1 = Number(prompt (' Insira seu primeiro número'))
+// let numero2 = Number(prompt (' Insira seu segundo número'))
+
+// let soma = numero1 + numero2
+// console.log(soma)
+
+// let multiplicar = numero1*numero2
+
